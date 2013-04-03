9db933591d9478ee8062e0c08366177f03ad5bab
Greg Wang <gregwym@users.noreply.github.com>
2013-04-03 05:22:10 -0400

    Able to print callgraph

--- src/ca/uwaterloo/pi/Main.java
+++ src/ca/uwaterloo/pi/Main.java
@@ -1,5 +1,9 @@
 package ca.uwaterloo.pi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
 	public Main() {
@@ -8,11 +12,16 @@ public class Main {
 
 	/**
 	 * @param args
+	 * @throws IOException 
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
-		System.out.println("#" + args.length + " args");
-		for(String arg: args) {
-			System.out.println(arg);
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Process tr = Runtime.getRuntime().exec( "opt -S -print-callgraph " + args[0] );
+		BufferedReader rd = new BufferedReader( new InputStreamReader( tr.getErrorStream() ) );
+		int exitcode = tr.waitFor();
+		while(rd.ready()) {
+			String s = rd.readLine();
+			System.out.println( s );
 		}
 	}
 
