de54db827b3ed7854fd78429c4169b3c17b7aa87
Greg Wang <gregwym@users.noreply.github.com>
2013-04-03 04:03:30 -0400

    Add build.xml, execution script and Main.java

---
+++ src/ca/uwaterloo/pi/Main.java
@@ -0,0 +1,19 @@
+package ca.uwaterloo.pi;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("#" + args.length + " args");
+		for(String arg: args) {
+			System.out.println(arg);
+		}
+	}
+
+}
